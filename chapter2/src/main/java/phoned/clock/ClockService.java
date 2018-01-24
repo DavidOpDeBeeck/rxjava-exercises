141920c3d25abc32940ba6f712c73674e155b6c9
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-01-24 21:09:18 +0100

    task 4: a naive implementation that notifies the subscriber every interval

--- chapter2/src/main/java/phoned/clock/ClockService.java
+++ chapter2/src/main/java/phoned/clock/ClockService.java
@@ -1,24 +1,49 @@
 package phoned.clock;
 
 import rx.Observable;
+import rx.Subscriber;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.lang.Thread.sleep;
 
 public class ClockService {
+
+    private static final int THREADS = 1;
+
     private Clock clock;
     private int tickInterval;
+    private ExecutorService executorService;
 
     public ClockService(Clock clock, int tickInterval) {
         this.clock = clock;
         this.tickInterval = tickInterval;
+        this.executorService = Executors.newFixedThreadPool(THREADS);
     }
 
     public void init() {
     }
 
     public Observable<LocalDateTime> getTime() {
-        //TODO Emit the time every tickInterval milliseconds
-        return Observable.just(LocalDateTime.now(clock));
+        return Observable.create(subscriber ->
+                executorService.submit(() -> notifySubscriberEveryInterval(subscriber)));
+    }
+
+    private void notifySubscriberEveryInterval(Subscriber<? super LocalDateTime> subscriber) {
+        try {
+            while (true) {
+                notifySubscriber(subscriber);
+                sleep(tickInterval);
+            }
+        } catch (InterruptedException e) {
+            subscriber.onError(e);
+        }
+    }
+
+    private void notifySubscriber(Subscriber<? super LocalDateTime> subscriber) {
+        subscriber.onNext(LocalDateTime.now(clock));
     }
 }
